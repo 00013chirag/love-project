409f647bd8b1b948d0285180f20110e2502f9366
00013chirag <150593926+00013chirag@users.noreply.github.com>
2026-05-12 02:44:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const noBtn =
+document.getElementById("noBtn");
+
+const yesBtn =
+document.getElementById("yesBtn");
+
+const result =
+document.getElementById("result");
+
+const music =
+document.getElementById("bgMusic");
+
+
+// MOVE NO BUTTON
+
+noBtn.addEventListener(
+  "mouseover",
+  () => {
+
+    const x =
+      Math.random() *
+      (window.innerWidth - 100);
+
+    const y =
+      Math.random() *
+      (window.innerHeight - 100);
+
+    noBtn.style.left = `${x}px`;
+
+    noBtn.style.top = `${y}px`;
+
+  }
+);
+
+
+// YES BUTTON CLICK
+
+yesBtn.addEventListener(
+  "click",
+  () => {
+
+    result.classList.remove("hidden");
+
+    music.play();
+
+    confetti({
+      particleCount:150,
+      spread:100,
+      origin:{ y:0.6 }
+    });
+
+  }
+);
